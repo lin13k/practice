b4e7d824b9ca11fbc64109a8e0b7532a33d26a3a
13k <jeremylin@rage.com.tw>
2017-04-14 14:16:07 +0800

    q4 for O(log(N+M))

--- algo_problems/q04.py
+++ algo_problems/q04.py
@@ -1,37 +1,44 @@
 class Solution(object):
     def findMedianSortedArrays(self, nums1, nums2):
+        m, n = len(nums1), len(nums2)
+        if m > n:
+            return self.findMedianSortedArrays(nums2, nums1)
+        a, b = nums1, nums2
 
-        n = len(nums1)
-        m = len(nums2)
-        for i in range((n + m) // 2):
-            if len(nums1) > 0 and len(nums2) > 0:
-                tmpInt = nums1.pop(0) if nums1[0] < nums2[0] else nums2.pop(0)
-            elif len(nums1) > 0:
-                tmpInt = nums1.pop(0)
+        min_i = i = 0
+        max_i = m
+        half_len = (m + n + 1) // 2
+        is_odd = True if (m + n) % 2 == 1 else False
+        while True:
+            i = (min_i + max_i) // 2
+            j = half_len - i
+            if i < m and a[i] < b[j - 1]:
+                min_i = i + 1
+            elif i > 0 and a[i - 1] > b[j]:
+                max_i = i - 1
             else:
-                tmpInt = nums2.pop(0)
+                if i == 0:
+                    maxLeft = b[j - 1]
+                elif j == 0:
+                    maxLeft = a[i - 1]
+                else:
+                    maxLeft = max(a[i - 1], b[j - 1])
+                if is_odd:
+                    return maxLeft / 1.0
 
-        if (n + m) % 2 == 1:
-            if len(nums1) > 0 and len(nums2) > 0:
-                tmpInt = nums1.pop(0) if nums1[0] < nums2[0] else nums2.pop(0)
-            elif len(nums1) > 0:
-                tmpInt = nums1.pop(0)
-            else:
-                tmpInt = nums2.pop(0)
-            return tmpInt / 1.0
-        else:
-
-            if len(nums1) > 0 and len(nums2) > 0:
-                tmpInt2 = nums1.pop(0) if nums1[0] < nums2[0] else nums2.pop(0)
-            elif len(nums1) > 0:
-                tmpInt2 = nums1.pop(0)
-            else:
-                tmpInt2 = nums2.pop(0)
-            return (tmpInt + tmpInt2) / 2.0
+                if i == m:
+                    minRight = b[j]
+                elif j == n:
+                    minRight = a[i]
+                else:
+                    minRight = min(a[i], b[j])
+                return (maxLeft + minRight) / 2.0
 
 
 if __name__ == '__main__':
+    print(Solution().findMedianSortedArrays([1, 2, 3], []), 2)
     print(Solution().findMedianSortedArrays([1, 3], [2]), 2)
     print(Solution().findMedianSortedArrays([1, 3], [2, 5]), 2.5)
     print(Solution().findMedianSortedArrays([1, 2], [3, 4]), 2.5)
     print(Solution().findMedianSortedArrays([], [3, 4]), 3.5)
+    print(Solution().findMedianSortedArrays([], [1,2,3,4,5]), 3)
