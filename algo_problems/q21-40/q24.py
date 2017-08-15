94f499b2ba0f51e19662f8a24d7888a2c3346007
13k <lin.13k@gmail.com>
2017-08-15 00:10:18 -0400

    q24

---
+++ algo_problems/q21-40/q24.py
@@ -0,0 +1,56 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+        def swapFunc(nodeHead, preNode, headFlag):
+            if nodeHead is not None:
+                if nodeHead.next is not None:
+                    nodeA = nodeHead
+                    nodeB = nodeHead.next
+                    nodeA.next = nodeB.next
+                    nodeB.next = nodeA
+                    if preNode:
+                        preNode.next = nodeB
+                    swapFunc(nodeA.next, nodeA, False)
+                    if headFlag:
+                        return nodeB
+                else:
+                    if headFlag:
+                        return nodeHead
+        head = swapFunc(head, None, True)
+        return head
+
+
+def genNodes(data):
+    tmpNode = None
+    preNode = None
+    for i in range(len(data)-1, -1, -1):
+        tmpNode = ListNode(data[i])
+        tmpNode.next = preNode
+        preNode = tmpNode
+    return tmpNode
+
+
+def NodesToList(dataNode):
+    head = dataNode
+    tmpLt = []
+    while head is not None:
+        tmpLt.append(head.val)
+        head = head.next
+    return tmpLt
+
+if __name__ == '__main__':
+
+    print(NodesToList(Solution().swapPairs(genNodes([1,2,3,4]))))
+    print(NodesToList(Solution().swapPairs(genNodes([1,2,3,4,5]))))
+    print(NodesToList(Solution().swapPairs(genNodes([1,2,3,4,5,6]))))
