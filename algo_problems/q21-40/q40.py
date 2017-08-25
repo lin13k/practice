61d107c73241c60e54ccbcc6d68e70c3022137ca
13k <lin.13k@gmail.com>
2017-08-24 23:44:04 -0400

    q40

---
+++ algo_problems/q21-40/q40.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        tmp = self.search(sorted(candidates), target, 0, (), [])
+        return [list(i) for i in list(set(tmp))]
+
+    def search(self, candidates, target, sumNumber, combination, result=[]):
+        if sumNumber == target:
+            result.append(combination)
+
+        if len(candidates) > 0:
+            if candidates[0] + sumNumber <= target:
+                self.search(candidates[1:], target, sumNumber +
+                            candidates[0], combination + (candidates[0],), result)
+                self.search(candidates[1:], target, sumNumber, combination, result)
+        return result
+
+
+if __name__ == '__main__':
+    print(Solution().combinationSum([2, 3, 6, 7], 7))
